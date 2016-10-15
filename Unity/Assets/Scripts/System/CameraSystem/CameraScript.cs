f6b1f3922557f7b134a848bbf5f7bc832db8a727
aldasack <3kocoa28@solent.ac.uk>
2016-10-15 14:45:46 +0100

    Added very basic protyp

---
+++ Unity/Assets/Scripts/System/CameraSystem/CameraScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CameraScript : MonoBehaviour {
+
+    ///<summary> MarbleCount, not needed at the moment </summary>
+    public short MarbleCount { get; private set; }
+    /// <summary> Stores all marbles in the level. </summary> Used for setting position of the camera.
+    private GameObject[] marbles;
+    private GameObject board;
+
+    // Use this for initialization
+	void Start () {
+        marbles = GameObject.FindGameObjectsWithTag("Marble");
+        MarbleCount = (short) marbles.Length;
+        board = GameObject.Find("Board");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Camera follows the marble with static distance
+        if (MarbleCount == 1)
+        {
+            Vector3 marblePos = marbles[0].transform.position;
+            Vector3 up = board.transform.up.normalized;
+            transform.position = marblePos + (up * 15);
+        }
+        
+        // TODO: Camera behavior with more than 1 marble
+    }
+}
---
+++ Unity/Assets/Scripts/System/PlayerMovementSystem/BoardMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // For Editor use
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if(h != 0)
+        {
+            transform.Rotate(transform.forward, 0.5f * h);
+        }
+        if(v != 0)
+        {
+            transform.Rotate(transform.right, 0.5f * v);
+        }
+
+        // For mobile devices
+        float x = Input.acceleration.x;
+        float z = Input.acceleration.z;
+        transform.Rotate(new Vector3(x, 0, z));
+
+	}
+}
