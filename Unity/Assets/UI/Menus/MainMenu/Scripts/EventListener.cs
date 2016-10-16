301f01397164ea0928ac087a47bb31da9fde71c8
3aniom97 <3aniom97@solent.ac.uk>
2016-10-16 19:00:55 +0100

    Added basic MainMenu, OptionsMenu, StartMenu and an EventListener for them

---
+++ Unity/Assets/UI/Menus/MainMenu/Scripts/EventListener.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EventListener : MonoBehaviour {
+
+	public GameObject mainMenu, startMenu, optionsMenu;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame()
+	{		
+		SceneManager.LoadScene ("prototyp");
+	}
+
+	public void EnableMainMenu()
+	{
+		this.mainMenu.SetActive (true);
+
+		if (this.optionsMenu.activeSelf)
+		{			
+			this.optionsMenu.SetActive (false);
+		}
+		if (this.startMenu.activeSelf)
+		{
+			this.startMenu.SetActive (false);
+		}
+	}
+
+	public void EnableStartMenu()
+	{
+		this.startMenu.SetActive (true);
+		this.mainMenu.SetActive (false);
+	}
+
+	public void EnableOptionsMenu()
+	{
+		this.optionsMenu.SetActive (true);
+		this.mainMenu.SetActive (false);
+	}
+
+
+
+}
