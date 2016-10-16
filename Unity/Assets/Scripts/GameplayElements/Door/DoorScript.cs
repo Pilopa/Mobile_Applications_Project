9bdf9ce885cca931e1d9a69cd29e277710341e24
aldasack <3kocoa28@solent.ac.uk>
2016-10-16 21:50:40 +0100

    Added basic trigger, needs some more work for correct behavior

---
+++ Unity/Assets/Scripts/GameplayElements/Door/DoorScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+    /// <summary>
+    /// Declares the direction in wich the door is opened
+    /// </summary>
+    public Vector3 MovingDirection { get; private set; }
+    public float Speed { get; private set; }
+    public float MovingDistance { get; set; }
+
+    public Vector3 StartPosition { get; private set; }
+
+	// Use this for initialization
+	void Start () {
+        MovingDirection = transform.forward;
+        Speed = 1;
+        MovingDistance = 1;
+        StartPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/Assets/Scripts/GameplayElements/Trigger/TriggerScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerScript : MonoBehaviour {
+    /// <summary>
+    /// The door which is opened and closed by this trigger
+    /// </summary>
+    public GameObject Door;
+    /// <summary>
+    /// Shows if marble is on the trigger
+    /// </summary>
+    private bool isActivated;
+    private DoorScript doorScript;
+
+	// Use this for initialization
+	void Start () {
+        doorScript = Door.GetComponent<DoorScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isActivated)
+        {
+            if(Vector3.Distance(Door.transform.position, doorScript.StartPosition) <= doorScript.MovingDistance)
+                Door.transform.position = Door.transform.position - doorScript.MovingDirection * doorScript.Speed * Time.deltaTime;
+        }
+        else
+        {
+            if (Vector3.Distance(Door.transform.position, doorScript.StartPosition) >= 0.1)
+                Door.transform.position = Door.transform.position + doorScript.MovingDirection * doorScript.Speed * Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Marble")
+        {
+            isActivated = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Marble")
+        {
+            isActivated = false;
+        }
+    }
+}
+
