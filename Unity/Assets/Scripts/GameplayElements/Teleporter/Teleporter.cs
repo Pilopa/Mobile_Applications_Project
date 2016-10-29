1ae0eb6fd57ba263c758d3abde9b119029ee1c14
aldasack <3kocoa28@solent.ac.uk>
2016-10-29 19:55:29 +0100

    Added teleporter

---
+++ Unity/Assets/Scripts/GameplayElements/Teleporter/Teleporter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+class Teleporter : MonoBehaviour
+{
+    /// <summary>
+    ///  Destination of the teleportation. This can be another teleporter
+    /// </summary>
+    public GameObject Destination;
+    /// <summary>
+    /// Time until marble is teleported
+    /// </summary>
+    public float TimeToTeleport = 3;
+
+    private Vector3 destination;
+    private float time;
+
+    void Start()
+    {
+        destination = Destination.transform.position;
+        time = 0.0f;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Marble")
+        {
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Marble")
+        {
+            time += Time.deltaTime;
+            if (time >= TimeToTeleport)
+            {
+                col.transform.position = destination;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {        
+        time = 0.0f;
+    }
+}
