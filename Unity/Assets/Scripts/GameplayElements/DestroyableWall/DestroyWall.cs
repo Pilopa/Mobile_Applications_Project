ebeabf2e1f63b0258a258930050c03fb875af4cd
Michael <3aniom97@solent.ac.uk>
2016-11-30 17:18:32 +0000

    Updated Destroyable Walls

--- Unity/Assets/Scripts/GameplayElements/DestroyableWall/DestroyWall.cs
+++ Unity/Assets/Scripts/GameplayElements/DestroyableWall/DestroyWall.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class DestroyWall : MonoBehaviour {
 
-	public float withstandingForce = 2;
+	public float withstandingForce = 4;
 
 
 	void OnCollisionEnter(Collision collision)
 	{
 		if (collision.gameObject.tag == "Marble") {
 			if (collision.impactForceSum.x * collision.rigidbody.mass > withstandingForce
-				| collision.impactForceSum.z * collision.rigidbody.mass > withstandingForce)
+				| collision.impactForceSum.y * collision.rigidbody.mass > withstandingForce
+				| collision.impactForceSum.z * collision.rigidbody.mass > withstandingForce
+			
+				|collision.impactForceSum.y * collision.rigidbody.mass < -withstandingForce	
+				|collision.impactForceSum.x * collision.rigidbody.mass < -withstandingForce
+				|collision.impactForceSum.z * collision.rigidbody.mass < -withstandingForce)
             {
                 this.GetComponent<Renderer> ().enabled = false;
 				this.GetComponent<Collider> ().enabled = false;			
